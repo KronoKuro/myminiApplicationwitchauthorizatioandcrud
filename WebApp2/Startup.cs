1b81995032223c4658b0dde69c0affd05802436a
Denis-Home <foxtrotdev@yandex.ru>
2018-10-20 06:23:41 +0300

    #change Startup

--- WebApp2/Startup.cs
+++ WebApp2/Startup.cs
@@ -1,12 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace WebApp2
 {
@@ -47,6 +47,8 @@ namespace WebApp2
                 });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
 
             services.AddCors(options =>
             {
